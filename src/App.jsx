c77d76001c69fa42e611f8ebcf44e864d6078492
Erik Truckner <eriktruckner@comcast.net>
2023-06-22 11:15:30 -0400

    Theatre.js Camera Animation Starter Template

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,59 @@
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { ScrollControls, useScroll } from '@react-three/drei'
+import { getProject, val } from '@theatre/core'
+import { SheetProvider, PerspectiveCamera, useCurrentSheet } from '@theatre/r3f'
 import FantasyBook from './modelComponents/FantasyBook'
 
+import flyThroughState from './fly.json'
+
 function App() {
+  const sheet = getProject('Fly Through', { state: flyThroughState }).sheet(
+    'Scene'
+  )
   return (
     <>
-      <Canvas camera={{ fov: 75, near: 1, far: 1000, position: [7, 15, 60] }}>
-        <color attach='background' args={['black']} />
-        <OrbitControls />
-        <ambientLight />
-        <FantasyBook />
+      <Canvas gl={{ preserveDrawingBuffer: true }}>
+        <ScrollControls pages={5} damping={1} maxSpeed={0.1}>
+          <SheetProvider sheet={sheet}>
+            <Scene />
+          </SheetProvider>
+        </ScrollControls>
       </Canvas>
     </>
   )
 }
 
 export default App
+
+//
+//
+
+function Scene() {
+  const sheet = useCurrentSheet()
+  const scroll = useScroll()
+
+  // our callback will run on every animation frame
+  useFrame(() => {
+    // the length of our sequence
+    const sequenceLength = val(sheet.sequence.pointer.length)
+    // update the "position" of the playhead in the sequence, as a fraction of its whole length
+    sheet.sequence.position = scroll.offset * sequenceLength
+  })
+
+  return (
+    <>
+      <color attach='background' args={['black']} />
+
+      <ambientLight />
+      <FantasyBook />
+      <PerspectiveCamera
+        theatreKey='Camera'
+        makeDefault
+        position={[0, 0, 0]}
+        fov={90}
+        near={0.1}
+        far={70}
+      />
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// import studio from '@theatre/studio'
+// import extension from '@theatre/r3f/dist/extension'
+
+// studio.extend(extension)
+// studio.initialize()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
+  </React.StrictMode>
 )
